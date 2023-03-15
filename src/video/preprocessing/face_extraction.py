d277e87d2e98516fa574100d4d61940e87e1671a
Denis <dresvyanskiydenis@gmail.com>
2023-03-16 00:06:51 +0100

    The script for face extraction has been slightly changed: new functions, corrections of the old ones, etc.

--- src/video/preprocessing/face_extraction.py
+++ src/video/preprocessing/face_extraction.py
@@ -9,13 +9,15 @@ import pandas
 import cv2
 import numpy as np
 from PIL import Image
+from moviepy.video.fx import crop
+from moviepy.video.io.VideoFileClip import VideoFileClip
 
 from src.video.preprocessing.face_extraction_utils import extract_face_according_bbox, recognize_faces_bboxes, \
     get_bbox_closest_to_previous_bbox, get_most_confident_person, load_and_prepare_detector_retinaFace_mobileNet
 
 
 def extract_face_one_video(path_to_video:str, detector:object, output_path:str, keep_the_same_person:Optional[bool]=False,
-                                 every_n_frame:Optional[int]=1)->pd.DataFrame: # TODO: check it
+                                 every_n_frame:Optional[int]=1)->pd.DataFrame:
     """ Extracts faces from video and saves them to the output path. Also, generates dataframe with information about the
     extracted faces.
 
@@ -66,9 +68,14 @@ def extract_face_one_video(path_to_video:str, detector:object, output_path:str, 
                                             f"{counter:05}.jpg")
                 # if not recognized, note it as NaN
                 if bboxes is None:
-                    # save previous face
-                    face = previous_face
-                    bbox = last_bbox
+                    # if there were no face vefore
+                    if previous_face is None:
+                        face = np.zeros((224,224,3), dtype=np.uint8)
+                        bbox = None
+                    else:
+                        # save previous face
+                        face = previous_face
+                        bbox = last_bbox
                 else:
                     # otherwise, extract the face and save it
                     if keep_the_same_person and last_bbox is not None:
@@ -130,7 +137,68 @@ def extract_faces_from_all_videos(paths_to_videos:List[str], detector:object, ou
         metadata_one_video = extract_face_one_video(path_to_video, detector, output_path, keep_the_same_person, every_n_frame)
         # add metadata from one video to the main metadata
         metadata = pd.concat([metadata, metadata_one_video], ignore_index=True, axis=0)
+        # print
+        print(f"Finished processing {path_to_video}")
+        # save metadata
+        metadata.to_csv(os.path.join(output_path, "metadata.csv"), index=False)
     return metadata
+
+def make_left_right_videos(path_to_data:str, output_path:str)->None:
+    # ration for train/dev/test videos to separate two different people
+    dividing_ratios = {
+            "46-30-484x360": 12.5/25.,
+            "6-30-1920x1080": 14./25.,
+            "video2":-1, # -1 means full frame for right and left
+            "video5":12.5/25.,
+            "video10_1":12.5/25.,
+            "video29":11.5/25.,
+            "video49": 10.5/25.,
+            "video55":12./25.,
+            "video59":12./25.,
+            "video74":12.5/25.,
+            "49-30-1280x720": 9.5/25.,
+            "135-24-1920x1080": 13.5/25.,
+            "130-25-1280x720":15.5/25.,
+    }
+    # get names of all videos with their extentions
+    all_videos = glob.glob(os.path.join(path_to_data, "*"))
+    all_videos = [os.path.basename(video) for video in all_videos]
+    # get dict filename->extention
+    all_videos = {os.path.splitext(video)[0]:os.path.splitext(video)[1] for video in all_videos}
+    # create output directory if needed
+    if not os.path.exists(output_path):
+        os.makedirs(output_path, exist_ok=True)
+    # extract faces from all videos
+    for filename, ratio in dividing_ratios.items():
+        # find idx of the video in all_videos list
+        full_path = os.path.join(path_to_data, filename+all_videos[filename])
+        # load video file using MoviePy
+        video = VideoFileClip(full_path)
+        # get height and width of the video
+        width, height = video.size
+        # get fps
+        fps = video.fps
+        # form new output names
+        output_name_left = os.path.join(output_path, filename+"_left"+all_videos[filename])
+        output_name_right = os.path.join(output_path, filename+"_right"+all_videos[filename])
+        # crop the video using ratio and Moviepy, then save it
+        if ratio == -1:
+            # if ratio=-1, then we need to save the full frame for both left and right videos
+            video_left = video
+            video_right = video
+        else:
+            # otherwise, we need to crop the video
+            video_left = crop.crop(video, x1=0, x2=width*ratio, y1=0, y2=height)
+            video_right = crop.crop(video, x1=width*ratio, x2=width, y1=0, y2=height)
+        # save videos
+        video_left.write_videofile(output_name_left, fps=fps)
+        video_right.write_videofile(output_name_right, fps=fps)
+
+
+
+
+
+
 
 
 
@@ -144,5 +212,15 @@ if __name__=="__main__":
     # load detector
     detector = load_and_prepare_detector_retinaFace_mobileNet()
     paths_to_videos = glob.glob(os.path.join(path_to_data, "*"))
-    extract_faces_from_all_videos(paths_to_videos=paths_to_videos, detector = detector, output_path=output_path,
+    metadata = extract_faces_from_all_videos(paths_to_videos=paths_to_videos, detector = detector, output_path=output_path,
                                   keep_the_same_person= True, every_n_frame= 1)
+    # save metadata
+    metadata.to_csv(os.path.join(output_path, "metadata.csv"), index=False)
+
+
+    # for creating the _left and _right videos
+    """
+    path_to_data = r"F:\Datasets\AffWild2\videos"
+    output_path = r"F:\Datasets\AffWild2\preprocessed\left_right_videos
+    make_left_right_videos(path_to_data, output_path)"""
+
--- src/video/preprocessing/utils.py
+++ src/video/preprocessing/utils.py
@@ -1,3 +1,5 @@
+import glob
+import os
 from typing import Tuple, List
 
 import pandas as pd
@@ -5,27 +7,32 @@ import torch
 import numpy as np
 
 
-
+# emotions order: Neutral,Anger,Disgust,Fear,Happiness,Sadness,Surprise,Other
+#                 0         1       2     3       4      5      6       7
 
 
 def get_names_of_train_dev_test_videos(path_to_train:str, path_to_dev:str, path_to_test_csv:str)->Tuple[List[str], List[str], List[str]]:
     """ Returns names of the videos that are used for training, validation and testing.
 
-    :param path_to_train_csv: str
-            path to the csv file with train videos
-    :param path_to_dev_csv: str
-            path to the csv file with dev videos
+    :param path_to_train: str
+            path to the dir with train txt annotations
+    :param path_to_dev: str
+             path to the dir with dev txt annotations
     :param path_to_test_csv: str
-            path to the csv file with test videos
+            path to the csv file with names of test videos
     :return: Tuple[List[str], List[str], List[str]]
             names of the videos that are used for training, validation and testing
     """
-    # load csv files
-    train_names_df = pd.read_csv(path_to_train_csv, header=None)
-    dev_names_df = pd.read_csv(path_to_dev_csv, header=None)
-    test_names_df = pd.read_csv(path_to_test_csv, header=None)
-    # get names of the videos
-    # TODO: get done it
-
+    # create train and dev names
+    train_videos = glob.glob(os.path.join(path_to_train, "*.txt"))
+    dev_videos = glob.glob(os.path.join(path_to_dev, "*.txt"))
+    # get only base names
+    train_videos = [os.path.basename(x) for x in train_videos]
+    dev_videos = [os.path.basename(x) for x in dev_videos]
+    # get rid of .txt extension
+    train_videos = [x.split(".")[0] for x in train_videos]
+    dev_videos = [x.split(".")[0] for x in dev_videos]
+    # get names of the test videos
+    test_videos = pd.read_csv(path_to_test_csv, header=None).iloc[:, 0].tolist()
 
     return train_videos, dev_videos, test_videos
