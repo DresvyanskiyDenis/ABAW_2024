b30c31d54b8700653b05a750468c29455fac7ae8
Denis <dresvyanskiydenis@gmail.com>
2023-03-15 10:03:13 +0100

    The preprocessing of the AffWIld2 videofiles has been added. Needs to be tested.

--- src/video/preprocessing/face_extraction.py
+++ src/video/preprocessing/face_extraction.py
@@ -9,10 +9,11 @@ import cv2
 import numpy as np
 from PIL import Image
 
-from src.video.preprocessing.utils import recognize_one_face_bbox, extract_face_according_bbox
+from src.video.preprocessing.utils import extract_face_according_bbox, recognize_faces_bboxes, \
+    get_bbox_closest_to_previous_bbox, get_most_confident_person, load_and_prepare_detector_retinaFace_mobileNet
 
 
-def extract_face_for_every_frame(path_to_video:str, detector:object, output_path:str, keep_the_same_person:Optional[bool]=False,
+def extract_face_one_video(path_to_video:str, detector:object, output_path:str, keep_the_same_person:Optional[bool]=False,
                                  every_n_frame:Optional[int]=1)->pd.DataFrame: # TODO: check it
     """ Extracts faces from video and saves them to the output path. Also, generates dataframe with information about the
     extracted faces.
@@ -41,6 +42,7 @@ def extract_face_for_every_frame(path_to_video:str, detector:object, output_path
     # go through all frames
     counter = 1
     previous_face = None
+    last_bbox = None
     while video.isOpened():
         ret, frame = video.read()
         if ret:
@@ -52,17 +54,25 @@ def extract_face_for_every_frame(path_to_video:str, detector:object, output_path
                 # round it to 2 digits to make it readable
                 timestamp = round(timestamp, 2)
                 # recognize the face
-                bbox = recognize_one_face_bbox(frame, detector)
+                bboxes = recognize_faces_bboxes(frame, detector, conf_threshold=0.8)
                 # create new filename for saving the face
                 new_filename = os.path.basename(path_to_video).split(".")[0]
                 new_filename = os.path.join(new_filename,
                                             f"{counter:05}.jpg")
                 # if not recognized, note it as NaN
-                if bbox is None:
+                if bboxes is None:
                     # save previous face
                     face = previous_face
                 else:
                     # otherwise, extract the face and save it
+                    if keep_the_same_person:
+                        # if we want to keep the same person, then we need to calculate the distances between the
+                        # center of the previous bbox and the current ones. Then, choose the closest one.
+                        bbox = get_bbox_closest_to_previous_bbox(bboxes, last_bbox)
+                    else:
+                        # otherwise, take the most confident one
+                        bbox = get_most_confident_person(bboxes)
+                    # extract face according to bbox
                     face = extract_face_according_bbox(frame, bbox)
                 # create full path to the output file
                 output_filename = os.path.join(output_path, new_filename)
@@ -78,6 +88,15 @@ def extract_face_for_every_frame(path_to_video:str, detector:object, output_path
             # increment counter
             counter += 1
             previous_face = face
+            last_bbox = bbox
         else:
             break
     return metadata
+
+
+
+if __name__=="__main__":
+    path_to_video = ""
+    path_to_output = ""
+    detector = load_and_prepare_detector_retinaFace_mobileNet()
+    metadata = extract_face_one_video(path_to_video, detector, path_to_output, keep_the_same_person=True, every_n_frame=1)
--- src/video/preprocessing/utils.py
+++ src/video/preprocessing/utils.py
@@ -4,7 +4,7 @@ and https://github.com/biubug6/Pytorch_Retinaface (main repository)
 Thanks to the authors for their work. For more details, please refer to the original repository.
 Do not forget to cite https://github.com/biubug6/Pytorch_Retinaface if you use RetinaFace in your work.
 """
-from typing import List, Union
+from typing import List, Union, Tuple
 from PIL import Image
 import numpy as np
 from retinaface import RetinafaceDetector
@@ -67,4 +67,46 @@ def extract_face_according_bbox(img:np.ndarray, bbox:List[float])->np.ndarray:
     return img[y1:y2, x1:x2]
 
 
+def recognize_faces_bboxes(img:Union[np.ndarray, str], detector:RetinafaceDetector.detect_faces, conf_threshold:float=0.8)->Tuple[List[float],...]:
+    """
+    Recognizes the faces in provided image and return the face with the highest confidence.
+    :param img: np.ndarray
+            image represented by np.ndarray
+    :param detector: object
+            the model, which has method detect. It should return bounding boxes and landmarks.
+    :return: List[float]
+            List of 5 floats, which represent the bounding box of face and its confidence
+    """
+    if type(img) is str:
+        img = np.array(Image.open(img))
+    recognized_faces = detector(img)
+    # filter faces with confidence less than threshold
+    recognized_faces = [face for face in recognized_faces if face[4] > conf_threshold]
+    recognized_faces = tuple(recognized_faces)
+    if recognized_faces is None or len(recognized_faces) == 0:
+        return None # no fases recognized
+    return recognized_faces
 
+
+def get_bbox_closest_to_previous_bbox(faces_bboxes:Tuple[List[float],...], previous_bbox:List[float])->List[float]:
+    """ Finds the bounding box, which is closest to the previous bounding box.
+
+    :param faces_bboxes: Tuple[List[float],...]
+            List of bounding boxes of faces, which were recognized by RetinaFace model
+    :param previous_bbox: List[float]
+            List of 5 floats, which represent the bounding box of face and its confidence
+    :return: List[float]
+            Closest to the previous_bbox bbox
+    """
+    # find the center of the previous bbox. The order of the coordinates is (x1, y1, x2, y2) = bbox[:4]
+    previous_bbox_center = ((previous_bbox[0]+previous_bbox[2])/2, (previous_bbox[1]+previous_bbox[3])/2)
+    # find the centers of all bboxes. The order of the coordinates is (x1, y1, x2, y2) = bbox[:4]
+    faces_bboxes_centers = ()
+    for bbox in faces_bboxes:
+        faces_bboxes_centers += ((bbox[0]+bbox[2])/2, (bbox[1]+bbox[3])/2)
+    # find the bbox, which is closest to the previous bbox based on the euclidean distance
+    distances = [np.linalg.norm(np.array(previous_bbox_center)-np.array(bbox_center)) for bbox_center in faces_bboxes_centers]
+    idx_closest_bbox = np.argmin(distances)
+    closest_bbox = faces_bboxes[idx_closest_bbox]
+
+    return closest_bbox
