7d544c280ff792fe4bd48878ac35aac5dc4f3689
Denis <dresvyanskiydenis@gmail.com>
2024-03-06 20:42:06 +0100

    Batch norm and v5 version have been added.

--- src/video/training/dynamic_models/dynamic_models.py
+++ src/video/training/dynamic_models/dynamic_models.py
@@ -136,6 +136,7 @@ class UniModalTemporalModel_v3(torch.nn.Module):
         self.num_timesteps, self.num_features = input_shape # (num_time_steps, num_features)
         self.num_classes = num_classes
         self.num_regression_neurons = num_regression_neurons
+        self.start_batch_norm = torch.nn.BatchNorm1d(num_features=self.num_features)
         self.__initialize_temporal_part()
 
         if self.num_classes is not None:
@@ -154,6 +155,10 @@ class UniModalTemporalModel_v3(torch.nn.Module):
 
 
     def forward(self, x):
+        # start batch norm
+        x = x.permute(0, 2, 1)
+        x = self.start_batch_norm(x)
+        x = x.permute(0, 2, 1)
         # temporal part
         x = self.first_transformer(x, x, x)
         x = self.second_transformer(x, x, x)
@@ -177,7 +182,7 @@ class UniModalTemporalModel_v4(torch.nn.Module):
         self.__initialize_temporal_part()
 
         if self.num_classes is not None:
-            self.classifier = torch.nn.Linear(in_features=self.num_features//2, out_features=self.num_classes)
+            self.classifier = torch.nn.Linear(in_features=self.num_features, out_features=self.num_classes)
         if self.num_regression_neurons is not None:
             self.regressor = torch.nn.Sequential(
                 torch.nn.Linear(in_features=self.num_features, out_features=self.num_regression_neurons),
@@ -186,7 +191,7 @@ class UniModalTemporalModel_v4(torch.nn.Module):
 
     def __initialize_temporal_part(self):
         # make the first part of the model as torch list
-        self.temporal_part = torch.nn.GRU(input_size=self.num_features, hidden_size=self.num_features//2, num_layers=2,
+        self.temporal_part = torch.nn.GRU(input_size=self.num_features, hidden_size=self.num_features, num_layers=2,
                                       batch_first=True, dropout=0.1, bidirectional=False)
 
 
@@ -202,11 +207,41 @@ class UniModalTemporalModel_v4(torch.nn.Module):
             return output
 
 
+class UniModalTemporalModel_v5(torch.nn.Module):
+    def __init__(self, input_shape:Tuple[int, int], num_classes:int, num_regression_neurons:int):
+        super(UniModalTemporalModel_v5, self).__init__()
+        self.num_timesteps, self.num_features = input_shape # (num_time_steps, num_features)
+        self.num_classes = num_classes
+        self.num_regression_neurons = num_regression_neurons
+        self.__initialize_temporal_part()
 
+        if self.num_classes is not None:
+            self.classifier = torch.nn.Linear(in_features=self.num_features, out_features=self.num_classes)
+        if self.num_regression_neurons is not None:
+            self.regressor = torch.nn.Sequential(
+                torch.nn.Linear(in_features=self.num_features, out_features=self.num_regression_neurons),
+                torch.nn.Tanh())
+
+
+    def __initialize_temporal_part(self):
+        # make the first part of the model as torch list
+        self.first_transformer = Transformer_layer(input_dim=self.num_features, num_heads=8, dropout=0.1, positional_encoding=True)
+
+
+    def forward(self, x):
+        # temporal part
+        x = self.first_transformer(x, x, x)
+        # output
+        if self.num_classes is not None:
+            output = self.classifier(x)
+            return output
+        elif self.num_regression_neurons is not None:
+            output = self.regressor(x)
+            return output
 
 
 if __name__ == "__main__":
-    model = UniModalTemporalModel_v2(input_shape=(20,256), num_classes=8, num_regression_neurons=2)
+    model = UniModalTemporalModel_v5(input_shape=(20,256), num_classes=8, num_regression_neurons=2)
     print(model)
     x = torch.rand(32, 20, 256)
     print(model(x))
