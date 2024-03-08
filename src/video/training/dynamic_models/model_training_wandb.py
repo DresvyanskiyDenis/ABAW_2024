93000cedd8223aaf148b06ad39961da42d9d3927
Denis <dresvyanskiydenis@gmail.com>
2024-03-08 13:36:47 +0100

    Different normalizations have been added.

--- src/video/training/dynamic_models/model_training_wandb.py
+++ src/video/training/dynamic_models/model_training_wandb.py
@@ -252,10 +252,11 @@ if __name__ == "__main__":
     parser.add_argument('--model_type', '-m', type=str, help='Type of the model. dynamic_v1, dynamic_v2, dynamic_v3, dynamic_v4', required=True)
     parser.add_argument('--challenge', type=str, help='Challenge. VA or Exp.', required=True)
     parser.add_argument('--window_size', '-w', type=int, help='Window size in frames (FPS=5)', required=True)
+    parser.add_argument('--normalization', '-n', type=str, help='Normalization type', required=True)
     args = parser.parse_args()
     # run main script with passed args
     main(args.path_to_config_file, window_size=args.window_size, stride=args.window_size*2//5, challenge=args.challenge,
-         model_type=args.model_type)
+         model_type=args.model_type, normalization = args.normalization)
     # clear RAM
     gc.collect()
     torch.cuda.empty_cache()
