fefc37f06a16e29e7497dbe45e96480e39adc75f
Denis <dresvyanskiydenis@gmail.com>
2024-03-13 23:02:25 +0100

    Test generation function is done. Only the paths to data should be fit.

--- src/video/post_processing/generate_test_predictions.py
+++ src/video/post_processing/generate_test_predictions.py
@@ -3,6 +3,8 @@ import os
 import sys
 from typing import List, Optional
 
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+
 path_to_project = os.path.abspath(
     os.path.join(os.path.dirname(__file__), os.path.pardir, os.path.pardir, os.path.pardir)) + os.path.sep
 sys.path.append(path_to_project)
@@ -94,8 +96,10 @@ def generate_test_predictions_one_video(df_video:pd.DataFrame, challenge, window
         # append everythign to the corresponding lists
         predictions_all.append(batch_predictions)
         pred_timesteps_all.append(timesteps)
-    # average predictions on timesteps (however, we need to prepare)
-    pred_timesteps_all, predictions_all = __average_predictions_on_timesteps([pred_timesteps_all, predictions_all])
+    # average predictions on timesteps (however, we need to prepare them to the format List[Tuple[np.ndarray, np.ndarray]])
+    # now they are List[np.ndarray]
+    preds_with_timesteps = list(zip(pred_timesteps_all, predictions_all))
+    pred_timesteps_all, predictions_all = __average_predictions_on_timesteps(preds_with_timesteps)
     # select the frame_nums depending on the prediction_timesteps
     pred_frame_nums = df_video.loc[df_video['timestep'].isin(pred_timesteps_all)]['frame_num'].values
     # round to two decimal places timesteps and frame_nums
@@ -104,7 +108,7 @@ def generate_test_predictions_one_video(df_video:pd.DataFrame, challenge, window
     # before interpolation, add the last timestep of the df_video to the pred_timesteps_all (duplicating the prediction)
     # as it can be that the last timestep has been removed due to the resampling
     if pred_timesteps_all[-1] != df_video['timestep'].values[-1]:
-        pred_timesteps_all = np.append(pred_timesteps_all, df_video['timestep'].values[-1], axis=0)
+        pred_timesteps_all = np.append(pred_timesteps_all, np.array([df_video['timestep'].values[-1]]), axis=0)
         predictions_all = np.append(predictions_all, [predictions_all[-1]], axis=0)
     # interpolate predictions to the 100 fps
     predictions_all, pred_timesteps_all = __interpolate_to_100_fps(predictions_all, pred_timesteps_all)
@@ -124,6 +128,9 @@ def generate_test_predictions_one_video(df_video:pd.DataFrame, challenge, window
     # check all parameters that we have the same number of frames
     assert predictions_all.shape[0] == df_video.shape[0]
 
+    # take the frame nums corresponding to timesteps
+    pred_frame_nums = df_video.loc[df_video['timestep'].isin(pred_timesteps_all)]['frame_num'].values
+
     return pred_frame_nums, pred_timesteps_all, predictions_all
 
 
@@ -134,19 +141,76 @@ def generate_test_predictions_one_video(df_video:pd.DataFrame, challenge, window
 
 def generate_test_predictions_all_videos(dynamic_model_type, path_to_weights, normalization, embeddings_columns,
                                input_shape, num_classes, num_regression_neurons, video_to_fps,
-                                 challenge, path_to_extracted_features:str, window_size:int, stride:int, device:torch.device,
-                                    batch_size:int=32):
+                               challenge, path_to_extracted_features:str, window_size:int, stride:int, device:torch.device,
+                               output_path:str,
+                               batch_size:int=32):
     # TODO: TBD as I do not know now the final format (test set has not been reliased yet)
     dynamic_model = __initialize_dynamic_model(dynamic_model_type=dynamic_model_type, path_to_weights=path_to_weights,
                                                input_shape=input_shape, num_classes=num_classes,
                                                num_regression_neurons=num_regression_neurons, challenge=challenge)
-    pass
+    dynamic_model = dynamic_model.to(device)
+    normalizer = MinMaxScaler() if normalization == "min_max" else StandardScaler()
+    # load metadata
+    metadata_static = glob.glob(os.path.join(path_to_extracted_features, "*.csv"))
+    metadata_static = {os.path.basename(file).split(".")[0]: pd.read_csv(file) for file in metadata_static}
+    # assign column names
+    columns = (['video_name', 'frame_num', 'timestep'] + [f"facial_embedding_{i}" for i in range(256)] +
+               [f"pose_embedding_{i}" for i in range(256)])
+    if challenge == "Exp":
+        columns = columns + ["category"]
+    else:
+        columns = columns + ["valence", "arousal"]
+    # assign column names to every dataframe
+    for video in metadata_static.keys():
+        metadata_static[video].columns = columns
+    # fit normalizer
+    # concatenate embeddings columns if tuple
+    feature_columns = embeddings_columns if not isinstance(embeddings_columns, tuple) else embeddings_columns[0] + \
+                                                                                           embeddings_columns[1]
+    features = np.concatenate(
+        [metadata_static[video][feature_columns].dropna().values for video in metadata_static.keys()], axis=0)
+    normalizer = normalizer.fit(features)
+    # process all videos
+    result = {}
+    for video in tqdm(metadata_static.keys()):
+        df = metadata_static[video]
+        # normalize features
+        df.loc[:, feature_columns] = normalizer.transform(df[feature_columns].values)
+        predictions = generate_test_predictions_one_video(df_video=df, challenge=challenge, window_size=window_size,
+                                                            stride=stride, model=dynamic_model, feature_columns=feature_columns,
+                                                            device=device, original_fps=video_to_fps[video], needed_fps=5,
+                                                            batch_size=batch_size)
+        result[video] = predictions
+    # save predictions to the
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+    for video in result.keys():
+        path_to_save = os.path.join(output_path, f"{video}.csv")
+        df_to_write = pd.DataFrame(np.concatenate([item.reshape(-1,1) for item in result[video]], axis=1),
+                                   columns=["frame_num", "timestep"] +
+                                           ["category"] if challenge=="Exp" else ["valence", "arousal"])
+        if challenge == "Exp":
+            df_to_write = df_to_write["category"].astype("int32")
+            # write first row as string "Neutral,Anger,Disgust,Fear,Happiness,Sadness,Surprise,Other" just using some writer
+            with open(path_to_save, 'w') as f:
+                f.write("Neutral,Anger,Disgust,Fear,Happiness,Sadness,Surprise,Other\n")
+        elif challenge == "VA":
+            df_to_write = df_to_write[["valence", "arousal"]]
+            # write first row as string "valence,arousal" just using some writer
+            with open(path_to_save, 'w') as f:
+                f.write("valence,arousal\n")
+
+        df_to_write.to_csv(path_to_save, mode='a', index=False, header=False)
+
+
+
+
 
 
 
 
 def main():
-    # static Exp
+    """# static Exp
     config_static_exp = {
         "static_model_type": "ViT_b_16",
         "pose_model_type": "HRNet",
@@ -179,14 +243,15 @@ def main():
         "batch_size": 32,
     }
     videos_test_VA = glob.glob(None) # TODO: add path to videos
-    process_all_videos_static_test(config=config_static_VA, videos=videos_test_VA)
+    process_all_videos_static_test(config=config_static_VA, videos=videos_test_VA)"""
 
 
     # make dynamic predictions
     config_dynamic_face_uni_modal_exp = {
-        "dynamic_model_facial": "dynamic_v3",
-        "normalization_face": "min_max",
-        "path_dynamic_model_facial": "/home/ddresvya/Data/weights_best_models/fine_tuned_dynamic/uni_modal_face_best.pth",
+        "dynamic_model_type": "dynamic_v3",
+        "embeddings_columns": [f"facial_embedding_{i}" for i in range(256)],
+        "normalization": "min_max",
+        "path_to_weights": "/home/ddresvya/Data/weights_best_models/fine_tuned_dynamic/uni_modal_face_best.pth",
         "input_shape": (20, 256),
         "num_classes": 8,
         "num_regression_neurons": None,
@@ -196,12 +261,10 @@ def main():
         "batch_size": 32,
         "challenge": "Exp",
         'video_to_fps': load_fps_file(os.path.join(path_to_project, "src/video/training/dynamic_models/fps.pkl")),
-        'output_path': None # TODO: add path to the output
+        'output_path': None, # TODO: add path to the output
+        'path_to_extracted_features': None # TODO: add path to the extracted features
     }
-    process_videos_Exp = glob.glob(None) # TODO: add path to videos
-    generate_test_predictions_all_videos(config_dynamic_face_uni_modal_exp, process_videos_Exp)
-
-
+    generate_test_predictions_all_videos(**config_dynamic_face_uni_modal_exp)
 
 
 
